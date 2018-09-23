331a764e45013ef823ebd1f7efdd0d512447e708
titiloxx <titiloxx@gmail.com>
2018-09-22 23:49:01 -0300

    Cambios visual studio

--- LavaWeb/App_Start/BundleConfig.cs
+++ LavaWeb/App_Start/BundleConfig.cs
@@ -22,9 +22,8 @@ namespace LavaWeb
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory("~/Content", "*.css", true));
+
         }
     }
 }
