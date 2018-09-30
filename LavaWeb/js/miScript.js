c9b6face06b3bfa4d5f0758ea9c0dca2f9b58336
Mario Merlano <31374800+mariomerlano@users.noreply.github.com>
2018-09-30 17:11:59 -0300

    adicion intento cambio en slider

---
+++ LavaWeb/js/miScript.js
@@ -0,0 +1,37 @@
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function sleepGD() {
+    await sleep(500);
+}
+async function sleepGrande() {
+    await sleep(5000);
+}
+
+    sleepGrande();
+    while (true) {
+        var string = $("#main_slider").attr("data-slideactive");
+        if (string != $("#main_slider").attr("data-slideactive")) {
+            var string2 = $("#main_slider").attr("data-slideactive");
+            break;
+        }
+        sleepGD();
+    }
+
+    while (true) {
+        if ($("#main_slider").attr("data-slideactive") == string2) {
+            $("#1").show();
+            $("#2").hide();
+        }
+        else {
+            $("#1").hide();
+            $("#2").show();
+        }
+        sleepGD();
+    }
+
+ 
+
+
